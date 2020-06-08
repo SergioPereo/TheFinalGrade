ad90a3bf254b594f3c378e0d966a3eda8304837d
carlosOchoa0699 <carlosochoa0699@gmail.com>
2020-06-08 01:31:56 -0500

    Pausa terminada

--- core/src/mx/itesm/thefinalgrade/TheFinalGrade.java
+++ core/src/mx/itesm/thefinalgrade/TheFinalGrade.java
@@ -216,7 +216,7 @@ public class TheFinalGrade extends Game {
 		manager.load("Sprites/platforms/Plataforma 1R_Mapa 3.png", Texture.class);
 		manager.load("Sprites/platforms/Plataforma 2R_Mapa 3.png", Texture.class);
 		manager.finishLoading();
-		setScreen(new StartMenu(this));
+		setScreen(new LoadingScreen(this));
 	}
 
 	@Override
